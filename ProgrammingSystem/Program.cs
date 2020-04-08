34325c22a7ab440f0d2ac087b1d33da5fed7ecda
Alex <Alex.Eremenko.Memento@gmail.com>
2020-04-08 13:48:48 +0300

    F

---
+++ ProgrammingSystem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleInjector;
+
+namespace ProgrammingSystem
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Init();
+			Application.Run(new Form1());
+		}
+
+		private static void Init()
+		{
+			var container = new Container();
+		}
+	}
+}
